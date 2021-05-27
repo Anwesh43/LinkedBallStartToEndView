e9975353c555e2a7b2a76274a1660b0508bb26f1
Anwesh43 <anweshthecool0@gmail.com>
2021-05-27 05:46:06 +0530

    adding Canvas extension methods

--- balltostartendview/src/main/java/com/example/balltostartendview/BallStartToEndView.kt
+++ balltostartendview/src/main/java/com/example/balltostartendview/BallStartToEndView.kt
@@ -28,3 +28,32 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBallStartToEnd(scale : Float, w : Float, h : Float, paint : Paint) {
+    val r : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-w / 2 - 2 * r, 0f)
+        drawLine(0f, 0f, (w / 2) * (sf.divideScale(j, parts) - sf.divideScale(j + 1, parts)), 0f, paint)
+        restore()
+    }
+    drawCircle(-r - w / 2 + (w / 2) * (sf1 + sf3), 0f, r, paint)
+    restore()
+}
+
+fun Canvas.drawBSTENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawBallStartToEnd(scale, w, h, paint)
+}
+
