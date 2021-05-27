6a5b3d982cce9320afb1c2cf6abb8c4010ed9b8b
Anwesh43 <anweshthecool0@gmail.com>
2021-05-27 05:33:28 +0530

    adding parts and importing libraries

--- balltostartendview/src/main/java/com/example/balltostartendview/BallStartToEndView.kt
+++ balltostartendview/src/main/java/com/example/balltostartendview/BallStartToEndView.kt
@@ -1,2 +1,25 @@
 package com.example.balltostartendview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#00C853",
+    "#1A237E",
+    "#FF6D00",
+    "#0091EA"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val strokeFactor : Float = 90f
+val scGap : Float = 0.02f / parts
+val delay : Long = 20
+val sizeFactor : Float = 3.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
