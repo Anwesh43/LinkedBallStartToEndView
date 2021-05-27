6d56ef2b0ff9140f4d1d8fdaf8edb8b4f3957b44
Anwesh43 <anweshthecool0@gmail.com>
2021-05-27 05:58:41 +0530

    adding BSTENode class

--- balltostartendview/src/main/java/com/example/balltostartendview/BallStartToEndView.kt
+++ balltostartendview/src/main/java/com/example/balltostartendview/BallStartToEndView.kt
@@ -119,4 +119,45 @@ class BallStartToEndView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BSTENode(var i : Int, val state : State = State()) {
+
+        private var next : BSTENode? = null
+        private var prev : BSTENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BSTENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBSTENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int,cb : () -> Unit) : BSTENode {
+            var curr : BSTENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
